08b26f0165357478141a785f736aafea7f54ce1b
shekabhi369 <shekabhi369@gmail.com>
2019-06-21 17:43:27 +0530

    extends SpringBootServletInitializer in SpringBootBackendServiceApplication class

--- src/main/java/com/gfg/abhishek/SpringBootBackendService/SpringBootBackendServiceApplication.java
+++ src/main/java/com/gfg/abhishek/SpringBootBackendService/SpringBootBackendServiceApplication.java
@@ -2,9 +2,10 @@ package com.gfg.abhishek.SpringBootBackendService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootBackendServiceApplication {
+public class SpringBootBackendServiceApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBackendServiceApplication.class, args);
